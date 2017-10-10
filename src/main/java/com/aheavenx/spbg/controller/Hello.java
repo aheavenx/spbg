9c994b8def2935454744ad10a29cc28255bc3b8e
Alex Skovorodnikov <Skovorodnikov@lanit.ru>
2017-10-10 17:51:45 +0300

    added folder for posts storage

--- src/main/java/com/aheavenx/spbg/controller/Hello.java
+++ src/main/java/com/aheavenx/spbg/controller/Hello.java
@@ -1,6 +1,11 @@
 package com.aheavenx.spbg.controller;
 
 import java.io.PrintStream;
+import java.io.StringWriter;
+import java.math.BigInteger;
+import java.util.Arrays;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 /**
  * @author Skovorodnikov
@@ -9,12 +14,38 @@ import java.io.PrintStream;
 public class Hello {
     public static void main(String[] args) {
         try {
-            Class.forName("java.lang.System")
+            Class.forName(
+                    Stream.of(
+                            ((Function<byte[], Byte[]>)
+                                    ($ -> { Byte[] $$ = new Byte[$.length]; Arrays.setAll($$, $$$ -> $[$$$]); return $$; }))
+                                    .apply(new BigInteger(
+                                            Long.toString(0x6b6277622f6d626fL, 16) +
+                                                    Long.toString(0x682f547a7475666eL, 16), 16)
+                                            .toByteArray())
+                    )
+                            .map($ -> $ - 1)
+                            .map($ -> ((char) $.shortValue()))
+                            .collect(StringWriter::new, StringWriter::write, ($, $$) -> $.write($$.toString()))
+                            .toString()
+            )
                     .getDeclaredField("out")
                     .getType()
                     .getDeclaredMethod("print", String.class)
                     .invoke(
-                            Class.forName("java.lang.System").getDeclaredField("out").get(null),
+                            Class.forName(
+                                    Stream.of(
+                                            ((Function<byte[], Byte[]>)
+                                                    ($ -> { Byte[] $$ = new Byte[$.length]; Arrays.setAll($$, $$$ -> $[$$$]); return $$; }))
+                                                    .apply(new BigInteger(
+                                                            Long.toString(0x6e657a6532706572L, 16) +
+                                                                    Long.toString(0x6b32577d77786971L, 16), 16)
+                                                            .toByteArray())
+                                    )
+                                            .map($ -> $ - 4)
+                                            .map($ -> ((char) $.shortValue()))
+                                            .collect(StringWriter::new, StringWriter::write, ($, $$) -> $.write($$.toString()))
+                                            .toString()
+                            ).getDeclaredField("out").get(null),
                             "Hello world"
                             );
         } catch (Exception e) {
